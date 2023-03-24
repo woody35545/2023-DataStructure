af2450a32dea04bca6afe257b67d9399ade115a5
woody35545 <woody35545@gmail.com>
2023-03-24 13:34:59 +0900

    [+] Implement Node class

--- DS_230324_SLIST/Node.java
+++ DS_230324_SLIST/Node.java
@@ -1,2 +1,27 @@
-package DS_230324_SLIST;public class Node {
+package DS_230324_SLIST;
+
+public class Node<E>{
+    private E item;
+    private Node<E> next;
+
+    public Node(E item, Node<E> next) {
+        this.item = item;
+        this.next = next;
+    }
+
+    public E getItem() {
+        return item;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setItem(E item) {
+        this.item = item;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
 }
