a24d1ee73c21c8b24853b34c4b97eb78681886e7
woody35545 <woody35545@gmail.com>
2023-04-07 20:50:16 +0900

    [+] Create package for submit

---
+++ DS_230407_TREE/SUBMIT/Node.java
@@ -0,0 +1,47 @@
+package DS_230407_TREE.SUBMIT;
+
+public class Node<Key extends Comparable<Key>> {
+
+    private Key item;
+    private Node<Key> left;
+    private Node<Key> right;
+
+
+    public Node(Key item, Node<Key> left, Node<Key> right) {
+        this.item = item;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Node() {
+    }
+
+    public Key getKey() {
+        return item;
+    }
+
+    public void setKey(Key item) {
+        this.item = item;
+    }
+
+    public Node<Key> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<Key> left) {
+        this.left = left;
+    }
+
+    public Node<Key> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<Key> right) {
+        this.right = right;
+    }
+
+    public void setLeftRight(Node<Key> left, Node<Key> right){
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ DS_230407_TREE/SUBMIT/Queue.java
@@ -0,0 +1,8 @@
+package DS_230407_TREE.SUBMIT;
+
+public interface Queue<E> {
+
+    void add(E element);
+    E remove();
+
+}
