ab0c371de13c609a5c5ba10dd262798de2d8a059
woody35545 <woody35545@gmail.com>
2023-04-15 03:07:18 +0900

    [+] Add SinglyList Study

---
+++ DS_230324_SLIST/study/Application.java
@@ -0,0 +1,34 @@
+package DS_230324_SLIST.study;
+
+public class Application {
+    public static void main(String args[]){
+        SinglyLinkedList<String> list = new SinglyLinkedList<>();
+
+
+        list.insertFront("A");
+        list.insertFront("B");
+        list.insertFront("C");
+        System.out.println(list.getHead().getNext().getNext());
+        list.print();
+
+        list.deleteBefore(list.getHead().getNext().getNext());
+        list.print();
+
+        // 주소가 같으면 그냥 같은 노드인건데
+        // 문제가 만약에 equals로 비교하면 이런일이 발생할 수 있음
+        Node root= new Node("Root", null);
+        Node a1 = new Node("A", root);
+        Node a2 = new Node("A", root);
+
+        System.out.println("node a1 at: " + a1);
+        System.out.println("node a2 at: " + a2);
+
+        System.out.println(a1==a2); // false
+        System.out.println(a1.getItem().equals(a2.getItem())); // true
+        System.out.println(root == a1.getNext());
+
+
+
+
+    }
+}
---
+++ DS_230324_SLIST/study/SinglyLinkedList.java
@@ -0,0 +1,148 @@
+package DS_230324_SLIST.study;
+import java.util.NoSuchElementException;
+
+public class SinglyLinkedList<E> implements List<E>{
+    private Node head;
+    private int size;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+
+    @Override
+    public int search(E target) {
+        int index = 0; // 위치를 기록하기 위한 변수
+
+        for(Node p = this.head; p.getItem() != null; p = p.getNext()){
+            if(p.getItem().equals(target)) return index;
+            index ++;
+        }
+
+        return -1; // 찾지 못한 경우 -1을 반환하도록 한다.
+    }
+
+    @Override
+    public void insertFront(E newItem) {
+        /* 1. 새로 생성할 노드의 next가 현재 head를 가리키게 한다. */
+        Node newNode = new Node(newItem, this.head);
+        /* 2. head를 방금 생성한 노드로 갱신한다. */
+        this.head = newNode;
+        /* size를 증가시킨다. */
+        this.size++;
+    }
+
+    @Override
+    public void insertBefore(E newItem, Node target) {
+        Node p = this.head;
+
+        while(true) {
+            //if(p.getNext().getNext()!=null && p.getNext().getNext().equals(target)) break;
+            if(p.getNext().getNext() == target) break;
+            p = p.getNext();
+        }
+
+        Node newNode = new Node(newItem, target);
+        p.setNext(newNode);
+        size ++;
+
+
+    }
+
+    @Override
+    public void insertAfter(E newItem, Node target) {
+        /* 1. 새로 만들 노드의 next가 t의 다음 노드(target.getNext())를 가리키게 한다.*/
+        Node newNode = new Node(newItem, target.getNext());
+        /* 2. 노드 t의 다음 노드(target.next)를 방금 생성한 노드를 가리키도록 갱신한다. */
+        target.setNext(newNode);
+        /* 3. size 변수를 증가시킨다. */
+        this.size++;
+    }
+
+    /* for test */
+    public E searchLast(){
+        Node<E> p = this.head;
+        for (int i=0; i<this.size-1; i++){
+            p = p.getNext();
+        }
+        return p.getItem();
+    }
+    @Override
+    public void deleteFront(){
+        /* 1. List가 비어있는지 확인한다. 비어있다면 수행하지 않는다. */
+        if(this.size == 0) throw new NoSuchElementException();
+        /* 2. 현재 head 값을 head의 next 값으로 갱신한다. */
+        this.head = this.head.getNext();
+        /* size 변수를 감소시킨다. */
+        size --;
+
+    }
+
+    @Override
+    public void deleteBefore(Node target) {
+
+        if(this.size == 0) throw new NoSuchElementException();
+
+        Node p = this.head;
+
+        while(p.getNext().getNext() != target){
+//            if(p.getNext().getNext() != null && p.getNext().getNext().equals(target))
+//            {
+//                break;
+//            }
+            p = p.getNext();
+
+        }
+
+        p.setNext(target);
+        target.setNext(null);
+        size --;
+
+    }
+
+    @Override
+    public void deleteAfter(Node target) {
+        if(this.size == 0 || target.getNext() == null) throw new NoSuchElementException();
+        target.setNext(target.getNext().getNext());
+        size --;
+    }
+
+    @Override
+    public void deleteLast() {
+        /* 1. List가 비어있는지 확인한다. 비어있다면 수행하지 않는다. */
+        if(this.size == 0) throw new NoSuchElementException();
+        /* 2. 마지막 노드의 이전 노드를 찾는다. */
+        Node currentNode = this.head;
+        while(currentNode.getNext() != null){
+            /* 마지막 노드의 getNext는 null 이므로 while loop이 종료되는 시점에
+               currentNode는 마지막 노드의 이전 노드가 된다.
+             */
+            currentNode = currentNode.getNext();
+        }
+        /* 3. 마지막 노드의 이전 노드의 next 값을 null 로 바꾸어 연결을 끊어준다. */
+        currentNode.setNext(null);
+
+        /* size 변수를 감소시킨다. */
+        this.size--;
+    }
+
+
+    public void print(){
+        Node<E> currentNode= head;
+        System.out.println("size: " + this.size);
+        for(int i=0; i<this.size; i++){
+            if(i == 0) System.out.print("(head)");
+            System.out.print(currentNode.getItem());
+
+            if(i < this.size-1) System.out.print(" -> ");
+            currentNode = currentNode.getNext();
+        }
+        System.out.println("\n");
+    }
+
+    public Node getHead(){
+        return this.head;
+    }
+
+}
