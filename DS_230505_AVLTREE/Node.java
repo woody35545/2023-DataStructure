a15a04b6752bfa4da241ea4eb4325b6ce15a72d4
woody35545 <woody35545@gmail.com>
2023-05-11 22:19:33 +0900

    [+] Implement AVL Tree

--- DS_230505_AVLTREE/Node.java
+++ DS_230505_AVLTREE/Node.java
@@ -1,11 +1,11 @@
 package DS_230505_AVLTREE;
 
 public class Node<Key, Value> {
-    private Key id;
-    private Value name;
-    private int height;
-    private Node left ,right;
-    private Node(Key newId, Value newValue, int newHeight){
+    public Key id;
+    public Value name;
+    public int height;
+    public Node left ,right;
+    public Node(Key newId, Value newValue, int newHeight){
         id = newId;
         name = newValue;
         height = newHeight;
