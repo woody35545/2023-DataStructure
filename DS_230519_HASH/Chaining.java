f0117b94d571b2cb9e2b3bb7d946b42d65a0c22a
woody35545 <woody35545@gmail.com>
2023-05-19 13:54:07 +0900

    [+] Implement Chaining

---
+++ DS_230519_HASH/Chaining.java
@@ -0,0 +1,51 @@
+package DS_230519_HASH;
+
+public class Chaining<K,V> {
+    protected int M = 13;
+    protected Node[] a = new Node[M];
+
+    public static class Node {
+        private Object key;
+        private Object data;
+        private Node next;
+
+        public Node(Object key, Object data, Node next) {
+            this.key = key;
+            this.data = data;
+            this.next = next;
+        }
+
+        public Object getKey() {
+            return key;
+        }
+
+        public Object getData() {
+            return data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+    }
+
+    protected int hash(K key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    protected void put(K key, V data) {
+        int i = hash(key);
+        for (Node x = a[i]; x != null; x = x.next)
+            if (key.equals(x.key)) {
+                x.data = data;
+                return;
+            }
+        a[i] = new Node(key, data, a[i]);
+    }
+
+    public V get(K key) {
+        int i = hash(key);
+        for (Node x = a[i]; x != null; x = x.next)
+            if (key.equals(x.key)) return (V) x.data;
+        return null;
+    }
+}
